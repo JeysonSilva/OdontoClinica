685ae2e65f30b9f3529625edfb4b44ca6b303b6e
Jeyson Jucemar da Silva <jeysonsw@gmail.com>
2022-07-15 21:14:18 -0300

    Versão 0.0.2 + Inicio Projeto + banco

---
+++ odontoclinica/src/test/java/br/com/senai/odontoclinica/OdontoclinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.odontoclinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontoclinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
